e564f94f4327ca19d1e66975f0a43dbef7e1c245
anouvene <nouveno@gmail.com>
2024-09-07 00:31:33 +0200

    Ajout commentaires test_login.py

--- test/admin/test_login.py
+++ test/admin/test_login.py
@@ -9,7 +9,8 @@ class TestLogin:
     def test_successful_login(self, driver):
         # Accéder à la page d'administration du site
         driver.get("http://localhost:3000/admin")
-        
+
+        # Cibler les champs du formulaire de connexion et le bouton submit
         email_input = driver.find_element(By.NAME, "email")
         password_input = driver.find_element(By.NAME, "password")
         login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
@@ -18,9 +19,9 @@ class TestLogin:
         email_input.send_keys("a.nouvene@it-students.fr")
         password_input.send_keys("jeTeste24$")
         login_button.click()
-    
+
+        # Vérif de connection au dashbord
         WebDriverWait(driver, 10).until(EC.title_contains("Dashboard"))
-        
         assert "Dashboard" in driver.title
 
 
